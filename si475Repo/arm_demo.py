8f5120f60c87a61dd35d77c29cbd62ce1210f5dc
si475 <m215394@gmail.com>
2021-01-28 14:37:01 -0500

    started proj

--- si475Repo/arm_demo.py
+++ si475Repo/arm_demo.py
@@ -1,11 +1,6 @@
 from arm_controller import ArmController
 
 ac=ArmController()
-ac.set_joints([.2,-.5,.3,-.4])
-print(ac.get_pose())
 ac.set_joints([0,0,0,0])
 print(ac.get_pose())
-ac.set_joints([-.2,.5,-.3,.4])
-print(ac.get_pose())
-ac.open_tool()
-print(ac.get_pose())
+
