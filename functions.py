45e81044ebdb41c464e2fdab6fd121880922dc5d
greg <m215394@usna.edu>
2021-01-28 11:03:27 -0500

    fixed functions

--- functions.py
+++ functions.py
@@ -1,18 +1,10 @@
 # Gregory Polmatier
 import numpy as np
-import math
 
-class Point:
-    def __init__(self, x, y):
-        self.pt = np.matrix([[x],[y],[1]])
+def trans(x,y):
+    return np.array([[1,0,x],[0,1,y],[0,0,1]])
 
-    def translate(self, m, n):
-        z = np.eye(3)
-        z[0,2] = m
-        z[1,2] = n
-        return np.dot(z, self.pt)
-
-    def rotate(self, theta):
-        z = np.matrix([[math.cos(theta), -math.sin(theta), 0], [math.sin(theta), math.cos(theta), 0], [0,0,1]])
-        return np.dot(z, self.pt)
-
+def rot(phi):
+    return np.array([[np.cos(phi), -np.sin(phi), 0],
+                    [np.sin(phi), np.cos(phi), 0],
+                    [0,0,1]])
