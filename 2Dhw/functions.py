21cdd23d6e6459b0f4dc05622ff5564cbd0d3626
greg <m215394@usna.edu>
2021-01-28 11:44:57 -0500

    file movement

--- 2Dhw/functions.py
+++ 2Dhw/functions.py
@@ -8,3 +8,9 @@ def rot(phi):
     return np.array([[np.cos(phi), -np.sin(phi), 0],
                     [np.sin(phi), np.cos(phi), 0],
                     [0,0,1]])
+
+def __main__():
+    origin = np.array([[0],[0],[1]])
+    phi1 = np.pi/4  
+    phi2 = -np.pi/6
+    print(np.eye(3).dot(rot(phi1)).dot(trans(5,0)).dot(rot(phi2)).dot(trans(3,0)))
