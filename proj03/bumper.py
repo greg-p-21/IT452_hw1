efc078ded32b2946755327d1875a4f97f8f8c972
Greg Polmatier <m215394@usna.edu>
2021-03-01 15:12:03 -0500

    bump

--- proj03/bumper.py
+++ proj03/bumper.py
@@ -1,5 +1,7 @@
 # Gregory Polmatier
 # For project 3 with Signorelli
+import random
+import math
 
 import rospy
 from turtleAPI import robot
@@ -10,7 +12,39 @@ try:
     # drive straight
     r.drive(angSpeed=0, linSpeed=.25)
     while not rospy.is_shutdown():
-        print(r.getBumpStatus())
+        bump = r.getBumpStatus()
+        if bump['status'] == 1:
+            if bump['bumper'] == 1:
+                if random.choice([True, False]):
+                    turn(math.pi/2)
+                else:
+                    turn(-math.pi/2)    
+            elif bump['bumper'] == 0:
+                turn(-math.pi/4)
+            else:
+                turn(math.pi/4)
+        
 except Exception as e:
     print(e)
     rospy.loginto("node now terminated")
+
+
+def turn(angle):
+    finalAngle = r.getAngle() + angle
+    
+    sign = 1
+    if angle < 0:
+        sign = -1
+
+    if angle == 0:
+        return
+
+    speed = .3
+    r.drive(angSpeed=speed*sign, linSpeed=0)
+    while diff(finalAngle, r.getAngle()) > .1:
+        print(r.getAngle, finalAngle)
+
+    return 
+
+def diff(a1, a2):
+    return abs(a1-a2)
