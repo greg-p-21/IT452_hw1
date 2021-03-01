59a9db9c4f9edd96eb16d2093c856fa875cefbd1
Greg Polmatier <m215394@usna.edu>
2021-03-01 13:41:59 -0500

    idk

--- proj02/inverseK.py
+++ proj02/inverseK.py
@@ -4,14 +4,18 @@
 # Grace Hove and Greg Polmatier
 
 import numpy as np
+from arm_controller import ArmController
 
 def makeT(x, y, z, psi, phi, theta):
     cpsi    = np.cos(psi)
     cphi    = np.cos(phi)
     ctheta  = np.cos(theta)
+    
     spsi    = np.sin(psi)
     sphi    = np.sin(phi)
     stheta  = np.sin(theta)
+
+    # print("angles", psi, phi, theta)
 
     return np.matrix([  [ctheta*cphi, -stheta*cpsi + ctheta*sphi*spsi,  stheta*spsi + ctheta*sphi*cpsi,  x],
                         [stheta*cphi,  ctheta*cpsi + stheta*sphi*spsi, -ctheta*spsi + stheta*sphi*cpsi,  y],
@@ -21,19 +25,17 @@ def makeT(x, y, z, psi, phi, theta):
 def getAngles(T):
     # lengths 
     D1 = 0.077
-    L2 = 0.1957
+    L2 = 0.195673197
     L3 = 0.126
 
     # angle 1
     a1 = np.arctan2(-T[0,1], T[1,1])
     
     # angle 2
-    # cos(a2)
-    # s2 = ((T[2,3] - D1) - T[2,0] * L3) / L2					
-    # # sin(a2)
-    # c2 = ((T[0,3]/np.cos(a1)) - (T[2,2] * L3)) / L2					
-    c2 = (T[0,3] - np.cos(a1)*L3*T[2,2])/ L2
-    s2 = (T[2,3] - L3* T[2,0] - D1) / L2
+    s2 = (T[2,3] - D1 - T[2,0] * L3) / L2	
+    # s2 = -1*(L3*T[0,2] + D1 - T[0,3]) / L2				
+    c2 = ((T[0,3]/T[1,1]) - (T[2,2] * L3)) / L2
+    # c2 = (T[0,3] - np.cos(a1)*L3*T[2,2])/ L2
     a2 = np.arctan2(s2, c2)
 
     # angle 3
@@ -52,11 +54,23 @@ if __name__ == "__main__":
 
     T = makeT(x, y, z, roll, pitch, yaw)
 
+    # print(T)
+
     theta1, theta2, theta3 = getAngles(T)
 
-    a1 = theta1 + 0
-    a2 = theta2 - .7131
-    a3 = theta3 + .7131
+    # print(theta1, theta2, theta3)
 
-    output = '\n(' + repr(a1) + ', ' + repr(a2) + ', ' + repr(a3) + ')'
-    print(output)
+    a1 = abs(theta1 + 0)
+    a2 = abs(theta2 - .7131)
+    a3 = abs(theta3 + .7131)
+
+    angles = [a1,a2,0,a3]
+    print(angles)
+
+
+    ac = ArmController()
+    ac.set_joints(angles)
+
+    # output = '\n(' + repr(a1) + ', ' + repr(a2) + ', ' + repr(a3) + ')'
+
+
