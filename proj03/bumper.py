3b320552d5b886050ae2741dbea526939d67af09
Greg Polmatier <m215394@usna.edu>
2021-03-01 19:33:05 -0500

    sadf

--- proj03/bumper.py
+++ proj03/bumper.py
@@ -13,7 +13,7 @@ try:
     r.drive(angSpeed=0, linSpeed=.25)
     while not rospy.is_shutdown():
         bump = r.getBumpStatus()
-        print(bump['status'])
+        print(type(bump))
         if bump['status'] == 1:
             if bump['bumper'] == 1:
                 if random.choice([True, False]):
