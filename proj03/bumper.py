92cc479a00b63bc17f2e32c569d0b73455788b04
Greg Polmatier <m215394@usna.edu>
2021-03-01 20:38:22 -0500

    hj

--- proj03/bumper.py
+++ proj03/bumper.py
@@ -30,7 +30,7 @@ def turn(angle):
 
     # print("deep in turn")
     RATE = rospy.Rate(10)
-    speed = .3
+    speed = .4
     # print("here now")
     R.drive(angSpeed=speed*sign, linSpeed=0)
     # print("1")
@@ -45,7 +45,7 @@ try:
     print("creating robot")
     RATE = rospy.Rate(10)
     # drive straight
-    R.drive(angSpeed=0, linSpeed=.25)
+    R.drive(angSpeed=0, linSpeed=.15)
     while not rospy.is_shutdown():
         RATE.sleep()
         bump = R.getBumpStatus()
@@ -55,15 +55,15 @@ try:
             if bump['bumper'] == 1:
                 if random.choice([True, False]):
                     print("got to turn\n\n\n")
-                    turn(math.pi/2 + .314)
+                    turn(math.pi/2)
                 else:
-                    turn(-math.pi/2-.314)    
+                    turn(-math.pi/2)    
             elif bump['bumper'] == 0:
-                turn(math.pi/4+.314)
+                turn(-math.pi/4-.314)
                 print("cw\n\n\n")
             else:
                 print("ccw\n\n\n")
-                turn(-math.pi/4 - .314)
+                turn(math.pi/4 + .314)
         R.drive(angSpeed=0, linSpeed=.25+.314)
         
 except Exception as e:
