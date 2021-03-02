15cb1f4a055d135c19f938b302c9d8f6452ff811
Greg Polmatier <m215394@usna.edu>
2021-03-01 19:41:45 -0500

    poop

--- proj03/bumper.py
+++ proj03/bumper.py
@@ -6,6 +6,28 @@ import math
 import rospy
 from turtleAPI import robot
 
+def diff(a1, a2):
+    return abs(a1-a2)
+
+def turn(angle):
+    finalAngle = r.getAngle() + angle
+    
+    sign = 1
+    if angle < 0:
+        sign = -1
+
+    if angle == 0:
+        return
+
+    print("deep in turn")
+
+    speed = .3
+    r.drive(angSpeed=speed*sign, linSpeed=0)
+    while diff(finalAngle, r.getAngle()) > .1:
+        print(r.getAngle, finalAngle)
+
+    return 
+
 try:
     print("creating robot")
     r = robot()
@@ -34,24 +56,5 @@ except Exception as e:
     # rospy.loginto("node now terminated")
 
 
-def turn(angle):
-    finalAngle = r.getAngle() + angle
-    
-    sign = 1
-    if angle < 0:
-        sign = -1
 
-    if angle == 0:
-        return
 
-    print("deep in turn")
-
-    speed = .3
-    r.drive(angSpeed=speed*sign, linSpeed=0)
-    while diff(finalAngle, r.getAngle()) > .1:
-        print(r.getAngle, finalAngle)
-
-    return 
-
-def diff(a1, a2):
-    return abs(a1-a2)
