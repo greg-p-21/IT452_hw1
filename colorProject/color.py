7858c7309f0a2a38aa2d925c287657db0f31ce0f
Greg Polmatier <m215394@usna.edu>
2021-03-11 11:08:02 -0500

    class

--- colorProject/color.py
+++ colorProject/color.py
@@ -1,44 +1,52 @@
 import cv2
 import numpy as np
 
-color_dict_HSV = {'black': [[180, 255, 30], [0, 0, 0]],
-              'white': [[180, 18, 255], [0, 0, 231]],
-              'red1': [[180, 255, 255], [159, 50, 70]],
-              'red2': [[9, 255, 255], [0, 50, 70]],
-              'green': [[89, 255, 255], [36, 50, 70]],
-              'blue': [[128, 255, 255], [90, 50, 70]],
-              'yellow': [[35, 255, 255], [25, 50, 70]],
-              'purple': [[158, 255, 255], [129, 50, 70]],
-              'orange': [[24, 255, 255], [10, 50, 70]],
-              'gray': [[180, 18, 230], [0, 0, 40]]}
+class Filter:
+    HSV_COLOR_RANGES = {
+        'red1': [[180, 255, 255], [159, 50, 70]],
+        'red2': [[9, 255, 255], [0, 50, 70]],
+        'green': [[89, 255, 255], [36, 50, 70]],
+        'blue': [[128, 255, 255], [90, 50, 70]],
+        'yellow': [[35, 255, 255], [25, 50, 70]],
+        'purple': [[158, 255, 255], [129, 50, 70]]
 
-img = cv2.imread('purple.png')
+    RGB_COLORS = {
+        'red': [0,0,255],
+        'green': [0,255,0],
+        'blue': [255,0,0],
+        'purple': [255,0,255],
+        'yellow': [0,255,255]
+    }
 
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    @staticmethod
+    def get_filtered(imgname, color):
+        img = cv2.imread(imgname)
 
-# Threshold of blue in HSV space
-# lower_blue = np.array([90, 50, 70])
-# upper_blue = np.array([128, 255, 255])
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# # Threshold of purple in HSV space
-# lower_green = np.array()
-# upper_green = np.array()
+        mask = None
+        if color == 'red':
+            mask1 = cv2.inRange(hsv, np.array(HSV_COLOR_RANGE['red1'][1]), np.array(HSV_COLOR_RANGE['red1'][0]))
+            mask2 = cv2.inRange(hsv, np.array(HSV_COLOR_RANGE['red2'][1]), np.array(HSV_COLOR_RANGE['red2'][0]))
 
-print(color_dict_HSV['purple'][1])
-# preparing the mask to overlay
-mask = cv2.inRange(hsv, np.array(color_dict_HSV['purple'][1]), np.array(color_dict_HSV['purple'][0]))
-    
-# The black region in the mask has the value of 0,
-# so when multiplied with original image removes all non-blue regions
-result = cv2.bitwise_and(img, img, mask = mask)
+            mask = mask1+mask2
+        else:
+            mask = cv2.inRange(hsv, np.array(HSV_COLOR_RANGE[color][1]), np.array(HSV_COLOR_RANGE[color][0]))
+            
+        # The black region in the mask has the value of 0,
+        # so when multiplied with original image removes all non-blue regions
+        # result = cv2.bitwise_and(img, img, mask = mask)
 
-aug = img
 
-aug[mask != 0] = [255,0,255]
+        img[mask != 0] = RGB_COLORS[color]
 
-cv2.imshow('frame', img)
-cv2.waitKey(0)
-cv2.imshow('result', result)
-cv2.waitKey(0)
-cv2.imshow('augmented', aug)
-cv2.waitKey(0)
+        return img
+
+if __name__ == '__main__':
+    yellow = Filter.get_filtered('yellow.png', 'yellow')
+    cv2.imshow("yellow", yellow)
+    cv2.waitKey(0)
+
+    red = Filter.get_filtered('red.png', 'red')
+    cv2.imshow('red', red)
+    cv2.waitKey(0)
