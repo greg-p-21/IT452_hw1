86758fef2a95790c9caf477b5004dd5b5a4bcddb
Greg Polmatier <m215394@usna.edu>
2021-03-11 10:11:21 -0500

    fg

--- colorProject/color.py
+++ colorProject/color.py
@@ -6,8 +6,8 @@ imgB = cv2.imread('blue.png')
 hsvB = cv2.cvtColor(imgB, cv2.COLOR_BGR2HSV)
 
 # Threshold of blue in HSV space
-lower_blue = np.array([60, 35, 140])
-upper_blue = np.array([180, 255, 255])
+lower_blue = np.array([90, 50, 70])
+upper_blue = np.array([128, 255, 255])
 
 # preparing the mask to overlay
 mask = cv2.inRange(hsvB, lower_blue, upper_blue)
