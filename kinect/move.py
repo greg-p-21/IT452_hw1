d696527f7ac3ba3f52e0c4461e49d3d3a90b96e7
Greg <m215394@usna.edu>
2021-03-22 14:34:32 -0400

    kinect

---
+++ kinect/move.py
@@ -0,0 +1,17 @@
+import rospy
+from turtleAPI import robot
+import cv2
+
+try:
+  print("creating robot")
+  r= robot()
+  r.drive(angSpeed=0, linSpeed=.2)
+  while not rospy.is_shutdown():
+    dpth=r.getDepth()
+    img=r.getImage()
+    cv2.imshow("Depth",dpth)
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
+except Exception as e:
+  print(e)
+  rospy.loginto("node now terminated")
