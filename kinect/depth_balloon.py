d54df60b78903fa5669ccf4f556866ba74a0c6d0
Greg Polmatier <m215394@usna.edu>
2021-03-24 13:54:17 -0400

    sdf

--- kinect/depth_balloon.py
+++ kinect/depth_balloon.py
@@ -5,8 +5,13 @@ from turtleAPI import robot
 from filter import Filter
 from pid import PID
 
+THRESHOLD = 30
+MAX_DISTANCE = 0.17
+ANGLE_SPEED = 0.2
+LINEAR_SPEED = 0.3
 
 try:
+    # start robot and ask for color
     colors = ['red', 'blue', 'green', 'purple', 'yellow']
     print("creating robot")
     r = robot()
@@ -20,37 +25,42 @@ try:
     if color not in colors:
         exit(1)
 
-    # prev_img = []
+    #create pid controller
     pid = PID(kp=.0006, ki=.00006, kd=.001)
     while not rospy.is_shutdown():
+        # get image and depth info
         img = r.getImage()
         depth=r.getDepth()
+        # create filter object
         flter = Filter(img, color, depth)
 
+        # get filter info 
         f_img = flter.get_filtered
         loc_val, num_detected = flter.get_PID_value
         mean_distance = flter.mean_distance
         print("distance", mean_distance)
 
+        # show image 
         cv2.imshow("Filtered Image", f_img)
         cv2.waitKey(1)
 
-        if num_detected > 30:
+        # make sure there is enough of the color there
+        if num_detected > THRESHOLD:
             pid_speed = pid(loc_val)
             # print(pid.components)
             found = True
         else:
             found = False
 
-        if mean_distance < .17:
+        # check if arrived at balloon
+        if mean_distance < MAX_DISTANCE:
             r.drive(angSpeed=0, linSpeed=0)
             print("arrived")
-        elif not found:
-            r.drive(angSpeed=0.2, linSpeed=0)
-        else:
-            r.drive(angSpeed=pid_speed, linSpeed=0.3)
+        elif not found: # Balloon not in view
+            r.drive(angSpeed=ANGLE_SPEED, linSpeed=0)
+        else: # Correct Balloon in image 
+            r.drive(angSpeed=pid_speed, linSpeed=LINEAR_SPEED)
         time.sleep(0.1)
-        #cv2.waitKey(1)
 
 except Exception as e:
   print(e)
